b2c74334cce949fd1e7b3a6ef3194d89af0f2fe8
Pablo Fernandez <fernandezpablo85@gmail.com>
2011-01-16 18:17:50 -0300

    Migrated from Specs to Scalatest/Specs

--- src/test/scala/streaming/BackOffSpec.scala
+++ src/test/scala/streaming/BackOffSpec.scala
@@ -1,38 +1,35 @@
 import com.linkedin.led.twitter.streaming._
 
-import org.specs._
+import org.scalatest._
+import org.scalatest.matchers._
 
-import org.specs.mock.Mockito
-import org.mockito.Mock._
-import org.mockito.Mockito._
-import org.mockito.Mockito.doNothing
+class BackOffSpec extends Spec with MustMatchers with BeforeAndAfterEach {
 
-object BackOffSpec extends Specification with Mockito {
-  "Back Off strategy" should {
+  var backOff: BackOff = null
 
-    var backOff: BackOff = null
+  override def beforeEach() {
+    backOff = BackOff(250, 16000)
+  }
 
-    doBefore {
-      backOff = BackOff(250, 16000)
+  describe("Back Off strategy") {
+
+    it ("should set the correct backOffTime and capBackOffAt") {
+      backOff.backOffTime must be === 250
+      backOff.capBackOffAt must be === 16000
     }
 
-    "set the correct backOffTime and capBackOffAt" in {
-      backOff.backOffTime mustEqual 250
-      backOff.capBackOffAt mustEqual 16000
+    it ("should increment the back off time incrementally") {
+      backOff.backOff
+      backOff.backOffTime must be === 500
+      backOff.backOff
+      backOff.backOffTime must be === 1000
     }
 
-    "increment the back off time incrementally" in {
+    it ("should allow to reset the incremental back off time") {
       backOff.backOff
-      backOff.backOffTime mustEqual 500
-      backOff.backOff
-      backOff.backOffTime mustEqual 1000
-    }
-
-    "allow to reset the incremental back off time" in {
-      backOff.backOff
-      backOff.backOffTime mustEqual 500
+      backOff.backOffTime must be === 500
       backOff.reset()
-      backOff.backOffTime mustEqual 0
+      backOff.backOffTime must be === 0
     }
   }
 }
--- src/test/scala/streaming/StreamingMethodsSpec.scala
+++ src/test/scala/streaming/StreamingMethodsSpec.scala
@@ -1,44 +1,39 @@
 import com.linkedin.led.twitter.streaming._
 
-import org.specs._
-
-import org.specs.mock.Mockito
-import org.mockito.Mock._
-import org.mockito.Mockito._
-import org.mockito.Mockito.doNothing
-import org.mockito.Matchers._
-
 import scala.collection.mutable.ArrayBuffer
 import org.apache.commons.httpclient._
 
-import org.apache.commons.httpclient.methods.PostMethod
-import org.apache.commons.httpclient.methods.GetMethod
+import org.apache.commons.httpclient.methods._
 
-object StreamingMethodsSpec extends Specification with Mockito {
-  "Streaming Methods" should {
+import org.scalatest._
+import org.scalatest.matchers.MustMatchers
 
-    var client: StreamingClientSpecHelper = null
-    var o: OutputStreamProcessor = null
+class StreamingMethodsSpec extends Spec with MustMatchers with BeforeAndAfterEach {
 
-    doBefore {
-      o = new OutputStreamProcessor
-      client = new StreamingClientSpecHelper("user", "password", o)
-    }
+  var client: StreamingClientSpecHelper = null
+  var o: OutputStreamProcessor = null
 
-    "build a Get request with specified params" in {
+  override def beforeEach() {
+    o = new OutputStreamProcessor
+    client = new StreamingClientSpecHelper("user", "password", o)
+  }
+
+  describe ("Streaming Methods") {
+
+    it ("should build a Get request with specified params") {
       val params = new ArrayBuffer[String]
         params += "count=10"
       val request = client.buildGet("www.linkedin.com", params)
-      request.getURI mustEqual new URI("www.linkedin.com?count=10")
+      request.getURI must be === new URI("www.linkedin.com?count=10")
     }
 
-    "build a post request with specified params" in {
+    it ("should build a post request with specified params") {
       val params = new ArrayBuffer[NameValuePair]
         params += new NameValuePair("follow", "linkedin,inapps,cool")
       val request = client.buildPost("www.linkedin.com", params)
-      request.getURI mustEqual new URI("www.linkedin.com")
-      request.getParameters.length mustEqual 1
-      request.getParameter("follow") mustEqual new NameValuePair("follow", "linkedin,inapps,cool")
+      request.getURI must be === new URI("www.linkedin.com")
+      request.getParameters.length must be === 1
+      request.getParameter("follow") must be === new NameValuePair("follow", "linkedin,inapps,cool")
     }
   }
 }
