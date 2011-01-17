f8948f338f67b6d322f50f1f0c19abbfa493467a
Pablo Fernandez <fernandezpablo85@gmail.com>
2011-01-16 22:29:20 -0300

    Use overloading version to avoid Header import

--- src/main/scala/com/linkedin/led/twitter/streaming/OAuthStreamingMethods.scala
+++ src/main/scala/com/linkedin/led/twitter/streaming/OAuthStreamingMethods.scala
@@ -2,20 +2,19 @@ package com.linkedin.led.twitter.streaming
 
 import scala.collection.mutable.ArrayBuffer
 import org.apache.commons.httpclient.NameValuePair
-import org.apache.commons.httpclient.Header
 import org.apache.commons.httpclient.methods.{PostMethod, GetMethod}
 
 trait OAuthStreamingMethods extends StreamingMethods {
 
   override def buildGet(url: String, params: ArrayBuffer[NameValuePair]) : GetMethod = {
     val get = super.buildGet(url, params)
-    get.addRequestHeader(new Header("Authorization", generateSignature(url, params)))
+    get.addRequestHeader("Authorization", generateSignature(url, params))
     return get
   }
 
   override def buildPost(url: String, params: ArrayBuffer[NameValuePair]) : PostMethod = {
     val post = super.buildPost(url, params)
-    post.addRequestHeader(new Header("Authorization", generateSignature(url, params)))
+    post.addRequestHeader("Authorization", generateSignature(url, params))
     return post
   }
 
